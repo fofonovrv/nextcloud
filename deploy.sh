8fa0a794148a79f0ba4102f9bb989f898c324a11
fofonovrv <92461655+fofonovrv@users.noreply.github.com>
2021-11-02 16:46:32 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Start deploying NextCloud"
+# https://rtfm.co.ua/nextcloud-zapusk-v-docker-compose-na-debian-s-ssl-ot-lets-encrypt/
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+echo "Installing packages"
+sudo apt-get instal -y lynx, net-tools
+
+echo "Installing Docker"
+curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker $USER 
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+echo "Installing pip, Docker-compose"
+sudo apt install -y pip
+sudo pip3 install docker-compose
+
+mkdir nextcloud
+cd nextcloud/
+mkdir -p nginx/{certs,vhost.d,html}
+mkdir mysql
+mkdir -p app/{config,custom_apps,data,themes,html}
+
+echo "Deploying docker-compose.yml"
+sudo docker-compose up -d
+echo "Complete!"
